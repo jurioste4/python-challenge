e35a65decfe37d00daccb87a4c814a93c1369be7
jurioste4 <jurioste4@gmail.com>
2019-03-01 19:24:00 -0600

    adding updated and finished code

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,8 +5,10 @@ rawdata=os.path.join('budget_data.csv')
 
 totalmonths = 0
 profit_loss = 0
+maxvalue, minvalue = [] , []
 #totalmonths = int(x[0])
 #profit_loss = int(x[1])
+
 
 with open(rawdata, 'r') as csvfile:
     
@@ -14,36 +16,32 @@ with open(rawdata, 'r') as csvfile:
     
     header = next(csvreader)
 
-   
-   #print(f"total months: {str(totalmonths)}")
     
-    #print(f"profit winLoss: {str(profit_loss)}")
-  
     for row in csvreader : 
         totalmonths += 1
         profit_loss += int(row[1])
+        minvalue.append(float(row[1]))
+        maxvalue.append(float(row[1]))
+        
+        #maxvalue = max(int(profit_loss))
+       
 
 print("total months:", totalmonths)
 print("profit total:",profit_loss)
 print("avearge total",profit_loss/totalmonths)
-        
-
-        
-
+print("min value",min(minvalue))
+print("max value:",max(maxvalue))
 
 
 
-   
-       
+test = os.path.join("pybanktest.txt")
 
+with open(test, 'w' , newline= '') as csvfile:
 
+    csvwriter = csv.writer(csvfile, delimiter=',')
 
-
-
-
-
-    #loop_increase = int(row[1])
-    #increase = loop_increase - greatest_increase
-
-
- 
+    csvwriter.writerow(["total Months:",totalmonths])
+    csvwriter.writerow(["Profit total:",profit_loss])
+    csvwriter.writerow(["Avearge Per Month:",profit_loss/totalmonths])
+    csvwriter.writerow(["min value:",min(minvalue)])
+    csvwriter.writerow(["max value:",max(maxvalue)])
